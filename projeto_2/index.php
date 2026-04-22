9161049eb4323dbdbe447a1f1482521ef90cc5d9
Filipe <filipevascq@GMAIL.COM>
2026-04-22 08:56:35 -0300

    Add files via upload

--- projeto_2/index.php
+++ projeto_2/index.php
@@ -1,38 +1,61 @@
+<?php
+session_start();
+require "redirecionar.php";
+require "banco.php";
+
+$id = $_SESSION['USU_CODIGO'];
+$stmt = $pdo->prepare("SELECT * FROM USUARIO WHERE USU_CODIGO = ?");
+$stmt->execute([$id]);
+
+$usuario = $stmt->fetch();
+?>
+
 <!DOCTYPE html>
-<html>
- <head>
+
+  <html lang="pt-br">
+
+    <head>
  
+     <link rel= "stylesheet" href="style.css">
+     <!-- guia o style para o index -->
 
-  <link rel= "stylesheet" href="style.css">
-  <!-- guia o style para o index -->
-
- </head>
-  <body>
+    </head>
+    <body>
   
-  <div id="s1" class= "a"></div><!--decoração superior-->
-  <div id="s2" class= "a"></div>
-  <div id="s3" class= "a"></div>
+     <div id="s1" class= "a"></div><!--decoração superior-->
+     <div id="s2" class= "a"></div>
+     <div id="s3" class= "a"></div>
 
-  <img src= "assets/logo.png" id="logo2" class= "a"></img><!--logo-->
+     <h1 id="ni" class="a">Seja Bem Vindo, <?= $usuario['USU_NOME']; ?></h1>
 
-   <button id="nr" class="a" onclick="novaredacao()"
-   > Nova Redação </button> <!--abre novaredacao-->  
+     <img src= "assets/logo.png" id="logo2" class= "a"></img><!--logo-->
+   
+      <div id="caixa" class="a"><!--caixa de botões-->
 
-  <button id="r" class="a" onclick="redacoes()"
-  > Redações </button><!--abre redacoes-->
-  
-  <script>
+       <button id="nr" class="a" onclick="novaredacao()"
+       > Nova Redação </button> <!--abre novaredacao-->  
 
-      function novaredacao(){
-      window.location.href= "novaredacao.php";
-      //cria o caminho de novaredacao//
-      };
-      function redacoes(){
-        window.location.href = "redacoes.php";
-        //cria o caminho de redacoes//
-      };
+       <button id="r" class="a" onclick="redacoes()"
+       > Redações </button> <!--abre redacoes-->
 
-  </script>
+       <button id="c" class="a" onclick="configuracoes()"
+       > Configurações </button> <!--abre configurações-->
 
- </body>
-</html>
+      </div>
+
+      <script>
+
+       function novaredacao(){
+       window.location.href= "novaredacao.php";
+       //cria o caminho de novaredacao//
+       };
+       function redacoes(){
+         window.location.href = "redacoes.php";
+         //cria o caminho de redacoes//
+       };
+
+      </script>
+
+    </body>
+
+  </html>
